c7b2f18becf089d1292ad7133a1ba812266d23e0
LAY <2748544112@qq.com>
2020-08-25 13:56:51 +0800

    修改了购物车

--- router/home/ShoppingcartPage.js
+++ router/home/ShoppingcartPage.js
@@ -4,13 +4,19 @@ const {User}=require("../../model/user");
 module.exports=async(req,res,next)=>{
   let titles=  await Title.find();
   let user=req.session.user
-
+  let xiaoji=[]
   if (user) {
     let resultCart=await Cart.find({uid:user._id})
+    resultCart.forEach((item,index) => {
+      let subtotal=item.subtotal
+      let cartCount=item.cartCount
+      xiaoji.push(subtotal*cartCount)
+    });
     res.render("home/Shoppingcart",{
       titles,
       user,
-      resultCart
+      resultCart,
+      xiaoji
     })
   }else{
     res.render("home/error",{
