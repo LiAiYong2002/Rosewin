937fa6bf05c3cf7536f1c313fa6f1b8c0cbff6ca
lzw <2959191487@qq.com>
2020-08-25 12:03:48 +0800

    lzw商品加载完成

--- router/admin/goodslist.js
+++ router/admin/goodslist.js
@@ -1,3 +1,8 @@
-module.exports=(req,res,next)=>{
-    res.render("admin/goodslist")
+const {Flower} = require('../../model/flower')
+const pagnation = require('mongoose-sex-page')
+module.exports=async(req,res,next)=>{
+    let {currentPagr} = req.query;
+    let result = await pagnation(Flower).find().page(currentPagr).size(10).display(5).exec();
+    console.log(result);
+    res.render("admin/goodslist",{result})
 }
