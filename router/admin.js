749e45ae0ecb93a0a19989c42d710744ca2fcfb1
lzw <2959191487@qq.com>
2020-08-25 09:18:26 +0800

    lzw骨架问题修改

--- router/admin.js
+++ router/admin.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const admin=express.Router()
-admin.get("/goodslist",require("./admin/userlist"));
+admin.get("/goodslist",require("./admin/goodslist"));
 admin.get("/goodsAdd",require("./admin/goodsAdd"));
 admin.get("/userlist",require("./admin/userlist"));
 admin.get("/userUpdate",require("./admin/userUpdate"));
