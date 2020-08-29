3890efd89bf87b841c3f1dc0a63dd7d2ccf960fa
LAY <2748544112@qq.com>
2020-08-29 20:17:52 +0800

    合并项目修改小问题

--- router/admin.js
+++ router/admin.js
@@ -2,22 +2,15 @@ const express= require("express");
 const admin=express.Router()
 admin.get("/goodslist",require("./admin/goodslist"));
 admin.get("/goodsAdd",require("./admin/goodsAdd"));
-admin.get("/deletegoods",require("./admin/Deletegoods"));
+// admin.get("/deletegoods",require("./admin/Deletegoods"));
 admin.post("/Addgoods",require("./admin/Addgoods"))
 admin.post("/updaategoods",require("./admin/updaategoods"))
 
 admin.get("/userlist",require("./admin/userlist"));
 admin.get("/userUpdate",require("./admin/userUpdate"));
-<<<<<<< HEAD
 
 admin.post("/updateUser",require("./admin/updateUser"))
 admin.get("/deleteUser",require("./admin/deleteUser"))
-admin.get("/deletegoods",require("./admin/Deletegoods"));
 
-admin.post("/Addgoods",require("./admin/Addgoods"))
-=======
-admin.post("/updateUser",require("./admin/updateUser"))
-admin.get("/deleteUser",require("./admin/deleteUser"))
 
->>>>>>> 65bb96ea69ffed99216f38b494958a3336dad107
 module.exports=admin;
