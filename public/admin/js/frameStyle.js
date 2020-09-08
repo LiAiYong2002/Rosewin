a84c02d9fa569795b649e27876ea3043d89484de
mbq <mbq@qq.com>
2020-09-08 11:00:54 +0800

    mbq删除完善

--- public/admin/js/frameStyle.js
+++ public/admin/js/frameStyle.js
@@ -4,23 +4,23 @@ function lodata(){
 lodata()
 
 
-window.addEventListener("load",function(){
-	var close = document.querySelector(".close")
-	var btnDefault = document.querySelector(".btn-default")
+// window.addEventListener("load",function(){
+// 	var close = document.querySelector(".close")
+// 	var btnDefault = document.querySelector(".btn-default")
 	
-	var modal = document.querySelector(".modal")
-	var modalLg = document.querySelector(".modal-lg")
-	var shanchu = document.querySelector(".shanchu")
-		shanchu.addEventListener('click',function(){
-			modal.style.display = 'block'
-			modalLg.style.display = 'block'
-		})
-		close.addEventListener('click',function(){
-			modal.style.display = 'none'
-			modalLg.style.display = 'none'
-		})
-		btnDefault.addEventListener('click',function(){
-			modal.style.display = 'none'
-			modalLg.style.display = 'none'
-		})
-})
+// 	var modal = document.querySelector(".modal")
+// 	var modalLg = document.querySelector(".modal-lg")
+// 	var shanchu = document.querySelector(".shanchu")
+// 		shanchu.addEventListener('click',function(){
+// 			modal.style.display = 'block'
+// 			modalLg.style.display = 'block'
+// 		})
+// 		close.addEventListener('click',function(){
+// 			modal.style.display = 'none'
+// 			modalLg.style.display = 'none'
+// 		})
+// 		btnDefault.addEventListener('click',function(){
+// 			modal.style.display = 'none'
+// 			modalLg.style.display = 'none'
+// 		})
+// })
